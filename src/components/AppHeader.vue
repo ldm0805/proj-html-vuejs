304e2721ce314c21efe578f5e1d01eb190c151d6
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-25 19:43:22 +0100

    Search added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -91,7 +91,12 @@ export default {
         <AppNav :menuNav="menuNav"/>
         <div class="shop">
             <ul>
-                <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a> </li>
+                <li>  
+                    <div class="searchInputWrapper">
+                        <input class="searchInput" type="text" placeholder='Ricerca'>
+                        <i class="searchInputIcon fa fa-search"></i>
+                    </div>
+                    </li>
                 <li><a href="#"><i class="fa-regular fa-user"></i></a> </li>
                 <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a><span>(0)</span> </li>
 
@@ -195,5 +200,50 @@ a:hover {
             }
         }
     }
+}
+
+.heading {
+    margin-bottom: 1.5rem;
+    font-size: 1.2rem;
+}
+
+.searchInputWrapper {
+    position: relative;
+}
+
+.searchInput {
+    width: 15rem;
+    height: 2rem;
+    padding: 0 1rem;
+    border-radius: 2rem;
+    border: none;
+    transition: transform 0.1s ease-in-out;
+}
+
+::placeholder {
+    color: #a1a1a1;
+}
+
+/* hide the placeholder text on focus */
+:focus::placeholder {
+    text-indent: -999px
+}
+
+.searchInput:focus {
+    outline: none;
+    transform: scale(1.1);
+    transition: all 0.1s ease-in-out;
+}
+
+.searchInputIcon {
+    position: absolute;
+    right: 0.8rem;
+    top: 0.5rem;
+    color: #a1a1a1;
+    transition: all 0.1s ease-in-out;
+}
+
+.container:focus-within>.searchInputWrapper>.searchInputIcon {
+    right: 0.2rem;
 }
 </style>
